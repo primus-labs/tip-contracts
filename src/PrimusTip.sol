47849dfc421b747c829a7f9eba718e69a862fc00
fksyuan <fksyuan@163.com>
2025-02-21 16:58:40 +0800

    add the PrimusTip contract and tip interface

---
+++ src/PrimusTip.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {TipRecipient} from "./types/Common.sol";
+
+/**
+ * @dev The Primus Tip contract is used to manage users’ tip funds.
+ *      Tippers can lock money in contracts, and recipients can claim the tip funds after verifying their identities.
+ */
+contract PrimusTip is OwnableUpgradeable {
+    /**
+     * @dev Initialize function to set the owner of the contract.
+     *      This function is called during the contract deployment.
+     * @param owner The contract owner.
+     */
+    function initialize(address owner) public initializer {
+        __Ownable_init(owner);
+    }
+
+    /**
+     * @dev Tipper tip the token to the recipient.
+     *      Tipper tip the native token when the token param equals to native.
+     * @param token The tip token.
+     * @param recipient The recipient informations include the identifier of the account and the platform.
+     */
+    function tip(string calldata token, TipRecipient calldata recipient) external payable {
+
+    }
+}
---
+++ src/types/Common.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+struct TipToken {
+    // Token Address.
+    address tokenAddress;
+    // Token Name.
+    address tokenName;
+}
+
+struct TipRecipient {
+    // The platform of the account. 
+    string idSource;
+    // The unique identifier of the account.
+    string id;
+}
