326f39aad9addb55caf874d626b918a8f665742a
WuEcho <emailforecho@163.com>
2025-02-27 09:40:15 +0800

    fix the test case bug

--- test/PrimusTip.t.sol
+++ test/PrimusTip.t.sol
@@ -44,17 +44,24 @@ contract ERC20Mock is IERC20 {
 
     function totalSupply() external view override returns (uint256) {}
 }
-
+contract PrimusZKTLSMock is IPrimusZKTLS {
+    function verifyAttestation(Attestation calldata) external pure override {
+        return;
+    }
+}
 contract PrimusTipTest is Test {
     PrimusTip public primusTip;
+    PrimusZKTLSMock public primusZKTLS;
     address public owner = address(1);
     address public tipper = address(2);
-    address public recipient = address(0x7ab44DE0156925fe0c24482a2cDe48C465e47573);
+    address public recipient = stringToAddress("0x7ab44DE0156925fe0c24482a2cDe48C465e47573");
     address public erc20Token = address(4);
     
     function setUp() public {
+        primusZKTLS = new PrimusZKTLSMock();
+
         primusTip = new PrimusTip();
-        primusTip.initialize(owner, IPrimusZKTLS(address(0)));
+        primusTip.initialize(owner, primusZKTLS);
         ERC20Mock token = new ERC20Mock();
         erc20Token = address(token);
         token.mint(tipper, 1000000); 
@@ -86,9 +93,9 @@ contract PrimusTipTest is Test {
         vm.stopPrank();
     }
 
-    function testClaimBySource() public {
+    function testClaimBySourceWithTiktok() public {
         string memory idSource = "tiktok";
-        string memory id = "user123";
+        string memory id = "fksyuan";
         
         TipToken memory token = TipToken({
             tokenType: "native",
@@ -116,8 +123,9 @@ contract PrimusTipTest is Test {
         });
 
         Attestor[] memory attesters = new Attestor[](1);
+        address addr = stringToAddress("0xdb736b13e2f522dbe18b2015d0291e4b193d8ef6");
         attesters[0] = Attestor({
-            attestorAddr:address(0xDB736B13E2f522dBE18B2015d0291E4b193D8eF6),
+            attestorAddr:addr,
             url:"https://primuslabs.xyz"
         });
 
@@ -129,7 +137,7 @@ contract PrimusTipTest is Test {
         });
 
         bytes[] memory sigBytes = new bytes[](1);
-        sigBytes[0] = abi.encodePacked("0x864997e50534f11042e2e8ad177d7d0bc9bbf49e050f04e89cf88071e2c2e3821dab1df547307881cc5572c5f2981e9545b2d861685844d63bf703f5381bfc201b");
+        sigBytes[0] = bytes("0x864997e50534f11042e2e8ad177d7d0bc9bbf49e050f04e89cf88071e2c2e3821dab1df547307881cc5572c5f2981e9545b2d861685844d63bf703f5381bfc201b");
         Attestation memory att = Attestation({
             recipient: recipient,
             request: request,
@@ -145,5 +153,79 @@ contract PrimusTipTest is Test {
         vm.startPrank(recipient);
         primusTip.claimBySource(idSource, att);
         vm.stopPrank();
+        assertEq(recipient.balance, 1 ether, "Native token transfer failed");
+    }
+
+    function testClaimBySourceWithX() public {
+        string memory idSource = "x";
+        string memory id = "wenjun_yuan1";
+        
+        TipToken memory token = TipToken({
+            tokenType: "native",
+            tokenAddress: address(0)
+        });
+        uint256[] memory nftIds = new uint256[](0);
+        TipRecipientInfo memory recipientInfo = TipRecipientInfo({
+            idSource: idSource,
+            id: id,
+            amount: 1 ether,
+            nftIds: nftIds
+        });
+        vm.startPrank(owner);
+        primusTip.addIdSource(idSource, "https://api.x.com/1.1/account/settings.json", "$.screen_name");
+        vm.stopPrank();
+        vm.deal(tipper, 10 ether);
+        vm.startPrank(tipper);
+        primusTip.tip{value: 1 ether}(token, recipientInfo);
+        vm.stopPrank();
+        AttNetworkResponseResolve[] memory response = new AttNetworkResponseResolve[](1);
+        response[0] = AttNetworkResponseResolve({
+            keyName: "screen_name",
+            parseType: "",
+            parsePath: "$.screen_name"
+        });
+
+        Attestor[] memory attesters = new Attestor[](1);
+        address addr = stringToAddress("0xdb736b13e2f522dbe18b2015d0291e4b193d8ef6");
+        attesters[0] = Attestor({
+            attestorAddr:addr,
+            url:"https://primuslabs.xyz"
+        });
+
+        AttNetworkRequest memory request = AttNetworkRequest({
+                url: "https://api.x.com/1.1/account/settings.json?include_ext_sharing_audiospaces_listening_data_with_followers=true&include_mention_filter=true&include_nsfw_user_flag=true&include_nsfw_admin_flag=true&include_ranked_timeline=true&include_alt_text_compose=true&ext=ssoConnections&include_country_code=true&include_ext_dm_nsfw_media_filter=true",
+                header: "",
+                method: "GET",
+                body: ""
+        });
+
+        bytes[] memory sigBytes = new bytes[](1);
+        sigBytes[0] = bytes("0xe0617d7d4b6f016aa68bed6d6afc8e1f28e3a2bf5ab53bd7822ceef9bef388b94f27097a1b3db5a451d3f7e3647e9f847c94e9de266b9194692a8e87519af8a01b");
+        Attestation memory att = Attestation({
+            recipient: recipient,
+            request: request,
+            reponseResolve: response,
+            data: "{\"screen_name\":\"wenjun_yuan1\"}", 
+            attConditions: "[{\"op\":\"REVEAL_STRING\",\"field\":\"$.screen_name\"}]",
+            timestamp:1740548090903,
+            additionParams:"{\"algorithmType\":\"proxytls\"}",
+            attestors: attesters,
+            signatures: sigBytes
+        });
+        
+        vm.startPrank(recipient);
+        primusTip.claimBySource(idSource, att);
+        vm.stopPrank();
+        assertEq(recipient.balance, 1 ether, "Native token transfer failed");
+    }
+
+      function stringToAddress(string memory _addressString) public pure returns (address) {
+        bytes memory addressBytes = bytes(_addressString);
+        require(addressBytes.length == 42, "Invalid address length");
+        address addr;
+        assembly {
+            addr := mload(add(_addressString, 20))
+        }
+        return addr;
     }
 }
